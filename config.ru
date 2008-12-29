3e78507b62f6f5fbd6f16bf1a06edd0a4c61ec0e
Server Administrator <serveradm@secure.kentfieldschools.org>
2008-12-29 13:10:18 -0800

    google analytics, too

--- config.ru
+++ config.ru
@@ -1,4 +1,5 @@
 require 'rack'
+require 'lib/google_analytics'
 require 'lib/cookie_auth'
 require 'lib/rack/mime'
 require 'lib/rack/index'
@@ -7,6 +8,7 @@ use Rack::CommonLogger
 use Rack::ShowExceptions
 
 # use Rack::Lint
+use GoogleAnalytics, 'UA-1439623-2'
 use CookieAuth, :auth_cookies => ['kwebauth', 'auth_ksdwebmin'], 
     :login_path => '/mgmt/xlogin', :mysql_db => 'ksdwebmin'
 run Rack::Index.new('/Library/WebServer/kentweb/teachers', '/static')
---
+++ lib/google_analytics.rb
@@ -0,0 +1,43 @@
+
+class GoogleAnalytics
+  def initialize(app, ga_acct_id)
+    @app = app
+    @end_body_re = Regexp.new('</body>', Regexp::IGNORECASE)
+    @ga_acct_id = ga_acct_id
+    @ga_script = <<END_SCRIPT
+<script type=\"text/javascript\">
+var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
+document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
+</script>
+<script type=\"text/javascript\">
+var pageTracker = _gat._getTracker(\"#{@ga_acct_id}\");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+</body>
+END_SCRIPT
+  end
+
+  def call(env)
+    status, headers, body = @app.call(env)
+    if status == 200 && (headers['Content-Type'] || '').index("text/") == 0 &&
+      body_s = ''
+      if body.is_a?(String)
+        body_s = body
+      else
+        body.each { |chunk| body_s << chunk }
+      end
+      if body_s.rindex(@ga_acct_id) == nil
+        m = @end_body_re.match(body_s)
+        if m
+          offset = m.offset(0)
+          body_s[offset[0], offset[1]-offset[0]] = @ga_script
+          headers['Content-Length'] = body_s.size.to_s
+          body = body_s
+        end
+      end
+    end
+    [status, headers, body]
+  end
+
+end
